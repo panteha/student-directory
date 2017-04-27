041413861a73f18b6d3afe14c18107ff75145591
Panteha <panteha.ahmadi@gmail.com>
2017-04-27 19:34:34 +0100

    Add exercise 8.7

--- directory.rb
+++ directory.rb
@@ -1,15 +1,30 @@
 def input_students
-  puts "Please enter the names of the student, hobbies, country of birth"
-  puts "Please enter the hobby"
+  puts "Please enter the names of the student, hobbies, country of birth, cohort:"
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp
   entry = name.split(",")
+
   while !entry.empty? do
-    students << {name: entry[0], hobby: entry[1], country: entry[2], cohort: :november}
+    for i in 0..2
+      if (entry[i] == "" || entry[i] == nil || entry[i] == " " )
+        entry[i] = "Blank"
+      end
+    end
+
+    if (entry[3] == "" || entry[3] == nil || entry[3] == " ")
+      entry[3] = "november"
+    end
+
+    students << {name: entry[0], hobby: entry[1],
+                 country: entry[2], cohort: entry[3].to_sym}
+
     puts "Now we have #{students.count} great students"
     name = gets.chomp
+
     entry = name.split(",")
+
+
   end
   students
 end
@@ -17,7 +32,9 @@ end
 def pick_letter(students)
   puts "Which specific letter do you choose?"
   letter = gets.chomp
+  puts "*****************************************************************************************"
   puts "List the students whose names start with letter #{letter.upcase} or #{letter.downcase} and are shorter than 12 characters"
+  puts "*****************************************************************************************"
   picked_student = []
   students.each do |student|
     picked_student << student if (student[:name].downcase.start_with? letter.downcase) && (student[:name].delete(" ").length < 12)
@@ -33,7 +50,7 @@ end
 def print(picked_student)
   i = 0
   while i < picked_student.length do
-        puts "#{i+1}. #{picked_student[i][:name].center(15, " ")}hobby:#{picked_student[i][:hobby].center(15, " ")}country of birth:#{picked_student[i][:country].center(15, " ")}(#{picked_student[i][:cohort]} cohort)"
+        puts "#{i+1}. #{picked_student[i][:name].center(15, " ")}hobby:#{picked_student[i][:hobby].center(15, " ")}country of birth:#{picked_student[i][:country].center(15, " ")}cohort:#{picked_student[i][:cohort].to_s.center(15, " ")}"
         i += 1
   end
 end
